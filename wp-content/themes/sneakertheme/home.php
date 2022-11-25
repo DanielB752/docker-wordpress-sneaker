9545f9996a11daf1b2295fe76e7946c45e5f73cf
DanielB752 <danielbeuing@gmx.de>
2022-11-25 20:03:55 +0100

    Update home.php

--- wp-content/themes/sneakertheme/home.php
+++ wp-content/themes/sneakertheme/home.php
@@ -14,109 +14,59 @@
             'category_name' => 'anleitungen',
             'posts_per_page' => 5,
         );
+        
+        $post_counter_anleitungen = 1;
 
         $arr_posts = new WP_Query($args);
 
         if($arr_posts->have_posts())
         {
             ?>
-                <ul>
+                <div class="home-articles-container">
                     <?php
                         while($arr_posts->have_posts())
                         {
                             $arr_posts->the_post();
 
-                            // if(has_post_thumbnail())
-                            // {
-                            //     the_post_thumbnail();
-                            // }
-                            ?><li><?php
-                            the_title();
-                            ?></li><?php
-                            // the_excerpt();
+                            if($post_counter_anleitungen === 1)
+                            {
+                                ?>
+                                    <div class="home-articles-container-top-post-container" style="background-image: url('<?php the_post_thumbnail_url() ?>')">
+                                        <div class="home-articles-container-top-post-container-title">
+                                            <?php the_title(); ?>
+                                        </div>
+                                        <div class="home-articles-container-top-post-container-excerpt">
+                                            <?php the_excerpt(); ?>
+                                        </div>
+                                    </div>
+                                <?php
+                                $post_counter_anleitungen ++;
+                            }
+                            else
+                            {
+                                ?>
+                                    <div class="home-articles-container-other-posts-container">
+                                        <div class="home-articles-container-other-posts-thumbnail">
+                                            <?php the_post_thumbnail(); ?>
+                                        </div>
+                                        <div class="home-articles-container-other-posts-content">
+                                            <div class="home-articles-container-other-posts-container-title">
+                                                <?php the_title(); ?>
+                                            </div>
+                                            <div class="home-articles-container-other-posts-container-excerpt">
+                                                <?php the_excerpt(); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php
+                            }
                         }
                     ?>
-                </ul>
+                </div>
             <?php
         }
-
+        $post_counter_anleitungen = 0;
         wp_reset_postdata();
     ?>
-
-
-<h2>Gadgets ><h2>
-    <?php
-        $args = array(
-            'post_type'     => 'post',
-            'post_status'   => 'publish',
-            'category_name' => 'gadgets',
-            'posts_per_page' => '5',
-        );
-
-        $arr_posts = new WP_Query($args);
-
-        if($arr_posts->have_posts())
-        {
-            ?>
-                <ul>
-                    <?php
-                        while($arr_posts->have_posts())
-                        {
-                            $arr_posts->the_post();
-
-                            // if(has_post_thumbnail())
-                            // {
-                            //     the_post_thumbnail();
-                            // }
-                            ?><li><?php
-                            the_title();
-                            ?></li><?php
-                            // the_excerpt();
-                        }
-                    ?>
-                </ul>
-            <?php
-        }
-
-        wp_reset_postdata();
-    ?>
-
-<h2>Artikel ><h2>
-    <?php
-        $args = array(
-            'post_type'     => 'post',
-            'post_status'   => 'publish',
-            'category_name' => 'artikel',
-            'posts_per_page' => '5',
-        );
-
-        $arr_posts = new WP_Query($args);
-
-        if($arr_posts->have_posts())
-        {
-            ?>
-                <ul>
-                    <?php
-                        while($arr_posts->have_posts())
-                        {
-                            $arr_posts->the_post();
-
-                            // if(has_post_thumbnail())
-                            // {
-                            //     the_post_thumbnail();
-                            // }
-                            ?><li><?php
-                            the_title();
-                            ?></li><?php
-                            // the_excerpt();
-                        }
-                    ?>
-                </ul>
-            <?php
-        }
-
-        wp_reset_postdata();
-    ?>
-
 
 <?php ?>
