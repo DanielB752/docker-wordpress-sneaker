2da95de57e5b863d4b6e93a0981a29db763942e4
FeltenDB <danielbeuing@gmx.de>
2022-12-08 15:45:29 +0100

    review element

--- wp-content/themes/sneakertheme/single.php
+++ wp-content/themes/sneakertheme/single.php
@@ -1,13 +1,77 @@
 <?php get_header(); ?>
+    <!-- ### POST THUMBNAIL ### -->
     <div class="single-post-thumbnail">
         <?php the_post_thumbnail(); ?>
     </div>
+
+    <!-- ### POST CONTENT ### -->
     <div class="single-post-content">
         <h1><?php the_title(); ?></h1>
-        <?php
-            the_content();
-        ?>
+        <?php the_content(); ?>
     </div>
+
+    <!-- ### RATING BOX ### -->
+    <?php 
+        if( has_term('gadgets', 'category') )
+        {
+            ?>
+                <div class="single-post-rating-container">
+                    <div class="single-post-rating-row-1">
+                        <div class="rating-grade">
+                            <h2>Fazit: <?php echo implode(get_field('mein_eindruck')); ?></h2>
+                        </div>
+                        <div class="rating-icon">
+                            <?php
+                                $input = implode(get_field('mein_eindruck'));
+                                $switchClass = '';
+                                switch ($input)
+                                {
+                                    case 'Ausgezeichnet':
+                                        $switchClass = 'icon-class-1';
+                                        break;
+                                    case 'Gut':
+                                        $switchClass = 'icon-class-2';
+                                        break;
+                                    case 'Ist einen Versuch wert':
+                                        $switchClass = 'icon-class-3';
+                                        break;
+                                    case 'Nutzlos':
+                                        $switchClass = 'icon-class-4';
+                                        break;
+                                }
+                            ?>
+
+                            <img class="<?php echo $switchClass; ?>" src="\wp-content\themes\sneakertheme\img\thumb.png">
+                        </div>
+                    </div>
+                    <div class="single-post-rating-row-2">
+                        <ul class="rating-pros">
+                            <li>
+                                <?php echo implode('</li><li>', get_field('pros')); ?>
+                            </li>
+                        </ul>
+                        <ul class="rating-cons">
+                            <li>
+                                <?php echo implode('</li><li>', get_field('cons')); ?>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="single-post-rating-row-3">
+                        <div class="rating-conlusion">
+                            <?php echo get_field('fazit'); ?>
+                        </div>
+                    </div>
+                    <div class="single-post-rating-row-4">
+                        <div class="rating-shop-link">
+                            <a href="<?php echo get_field('shoplink'); ?>" target="_blank" rel="nofollow" title="zum Gadgetshop"><button class="review-button">zum Gadgetshop</button></a>
+                        </div>
+                    </div>    
+                </div>
+            <?php
+        }
+    ?>
+
+    <!-- ### RELATED POSTS ###  -->
     <div class="single-post-related-items">
         <?php 
             $args = array(
